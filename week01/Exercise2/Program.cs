c6ec05230dfd16ac0d8cf2172345213513958b68
rod-soriano-work <roso.pblx@gmail.com>
2025-05-11 09:49:17 -0600

    Implement grade calculation and feedback based on user input

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard! You'll get it next time.");
+        }
+
+        string sign = "";
+        if (letter != "F")
+        {
+            int lastDigit = gradePercentage % 10;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
     }
 }
